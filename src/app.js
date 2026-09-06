4c6544fb980538b07b4df7e31006de2d86f6241f
davit412v2 <luisdavidbarrigagaray97@gmail.com>
2026-09-06 13:01:47 -0500

    fix cors

--- src/app.js
+++ src/app.js
@@ -4,10 +4,14 @@ import taskRouter from "../src/routes/TaskRouter.js";
 import AuthRouter from "../src/routes/AuthRouter.js";
 import { errorMiddleware } from "./middleware/ErrorMiddleware.js";
 import { authMiddleware } from "./middleware/AuthMiddleware.js";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors({
+    origin: "http://localhost:5173"
+}));
 app.use("/auth", AuthRouter);
 app.use(authMiddleware);
 app.use("/projects", projectRouter);
